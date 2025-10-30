2365c95f058bd5b60a2da21eab889def0db0ac62
Divyanshu1117 <vashishthdivyanshu18@gmail.com>
2025-10-30 22:23:59 +0530

    Add new programs

--- 161_Exercise_3.py
+++ 161_Exercise_3.py
@@ -1,22 +1,57 @@
 class Library:
-    def __init__ (self, listOfBooks):
+    def __init__(self, listOfBooks):
         self.books = listOfBooks
-        
+
     def displayAvailavleBooks(self):
         print("Books present in this library are: ")
         for book in self.books:
             print("\t" + book)
-            
+
     def borrowBook(self, bookName):
         if bookName in self.books:
-           print(f"You have been issued {bookName}. Please keep it safe and return it within 30 days")
-           self.books.remove(bookName)   
+            print(f"You have been issued {bookName}. Please keep it safe and return it within 30 days")
+            self.books.remove(bookName)
+            return True
         else:
-           print("Sorry, This book has already been issued to someone else. Please wait until the book is returned")        
-            
+            print("Sorry, This book is either not available or has already been issued to someone else. Please wait until the book is available")
+            return False
+
+    def returnBook(self, bookName):
+        self.books.append(bookName)
+        print("Thanks for returning this book! Hope you enjoyed reading it. Have a great day ahead!")
+        
 class Student:
-    pass
+    def requestBook(self):
+        self.book = input("Enter the name of the book you want to borrow: ")
+        return self.book
+
+    def returnBook(self):
+        self.book = input("Enter the name of the book you want to return: ")
+        return self.book
 
 if __name__ == "__main__":
     centralLibrary = Library(["Algorithms", "Django", "Clrs", "Python Notes"])
-    centralLibrary.displayAvailavleBooks()
+    student = Student()
+    # centralLibrary.displayAvailavleBooks()
+
+    while True:
+        WelcomeMsg = """ \n ====== Welcome to Central Library ======
+        Please choose an option:
+        1. List all the books
+        2. Request a book
+        3. Add/Return a book 
+        4. Exit the Library
+        """
+        print(WelcomeMsg)
+        a = int(input("Enter a choice: "))
+        if (a == 1):
+            centralLibrary.displayAvailavleBooks()
+        elif (a == 2):
+            centralLibrary.borrowBook(student.requestBook())
+        elif (a == 3):
+            centralLibrary.returnBook(student.returnBook())
+        elif (a == 4):
+            print("Thanks for choosing Central Library. Have a great day ahead!")
+            exit()
+        else:
+            print("Invalid Choice! ")
